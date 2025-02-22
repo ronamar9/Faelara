5ef644919584574ee6997bf491b9f6ef102ca84c
ronamar9 <ronamar9@gmail.com>
2025-02-22 21:16:00 +0200

    Update Bear Crush

--- Assets/Scripts/Rock/Rock.cs
+++ Assets/Scripts/Rock/Rock.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 public class Rock : MonoBehaviour
 {
     private int life = 3;
+    private int hits = 0;
+    [SerializeField] private Sprite[] sprites = new Sprite[4];
+    private SpriteRenderer sr;
 
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = sprites[0];
+    }
     public void GetDamage()
     {
-        life--;
-        print(life);
-        if (life <= 0)
+        hits++;
+        if (hits >= life)
         {
             Crush();
         }
+        sr.sprite = sprites[hits];
     }
 
     private void Crush()
